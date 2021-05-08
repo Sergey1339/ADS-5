fefda435369d51aa643508ab5e30172f3e1bc1e7
Sergey1339 <79190170+Sergey1339@users.noreply.github.com>
2021-05-08 17:07:30 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,7 +5,56 @@
 
 template<typename T>
 class TPQueue {
-  // Сюда помещается описание структуры "Очередь с приоритетами"
+ private:
+  T core;
+  TPQueue* next;
+  TPQueue* prev;
+  TPQueue* begin;
+  TPQueue* end;
+ public:
+  TPQueue() {
+    next = NULL;
+    prev = NULL;
+    begin = NULL;
+    end = NULL;
+  }
+ void push(const T &element) {
+    if (begin != NULL) {
+      TPQueue* step = new TPQueue; // создание нового элемента
+      step->core = element;        //захват значения 
+      step->prev = prev;           //cвязь с предыдущем элементом
+      prev = step;
+      next->next = step;           //cвязь с предыдущем элементом
+      next = step;
+      end = step;
+      if (step->core.prior > step->prev->core.prior) {
+        TPQueue* step_prev = step->prev;
+        TPQueue* step_this = step;
+        while (step_prev && step_this->core.prior > step_prev->core.prior) {
+          T x = step_this->core;
+          step_this->core = step_prev->core;
+          step_prev->core = x;
+          step_this = step_this->prev;
+          step_prev = step_prev->prev;
+        }
+      }
+    } else {
+      TPQueue* step = new TPQueue;
+      step->core = element;
+      begin = step;
+      prev = step;
+      next = step;
+    }
+  }
+ char pop() {
+   if (begin != NULL) {
+     char x = begin->core.ch;
+     begin = begin->next;
+     return x;
+   } else {
+     return '/';
+   }
+ }
 };
 
 struct SYM {
